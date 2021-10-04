2d547be0f092591176b149c469510656d7d1f6c7
seiya0412 <seiya.shimizu@whill.inc>
2021-10-04 22:51:09 +0900

    apply formatter

--- ros_awsiot_agent/test/listener.py
+++ ros_awsiot_agent/test/listener.py
@@ -2,9 +2,11 @@
 import rospy
 from std_msgs.msg import String
 
+
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    
+
+
 def listener():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
@@ -12,12 +14,13 @@ def listener():
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    rospy.init_node("listener", anonymous=True)
 
     rospy.Subscriber("chatter", String, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     listener()
--- ros_awsiot_agent/test/talker.py
+++ ros_awsiot_agent/test/talker.py
@@ -3,17 +3,19 @@
 import rospy
 from std_msgs.msg import String
 
+
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(0.2) # once in 5sec
+    pub = rospy.Publisher("chatter", String, queue_size=10)
+    rospy.init_node("talker", anonymous=True)
+    rate = rospy.Rate(0.2)  # once in 5sec
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         talker()
     except rospy.ROSInterruptException:
