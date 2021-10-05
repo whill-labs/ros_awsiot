2f39d401102b30aac365e8cb5f2a6855ee1280e1
seiya0412 <seiya.shimizu@whill.inc>
2021-10-05 22:56:44 +0900

    update test messages

--- ros_awsiot_agent/test/talker.py
+++ ros_awsiot_agent/test/talker.py
@@ -7,9 +7,9 @@ from std_msgs.msg import String
 def talker():
     pub = rospy.Publisher("chatter", String, queue_size=10)
     rospy.init_node("talker", anonymous=True)
-    rate = rospy.Rate(0.2)  # once in 5sec
+    rate = rospy.Rate(0.1)  # once in 10sec
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
+        hello_str = "Hello AWS IoT, greetings from ROS! %s" % rospy.get_time()
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
