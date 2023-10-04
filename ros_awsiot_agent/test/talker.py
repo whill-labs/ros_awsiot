140fd707aa6c596a16639f1d3d1823bcf2f66c59
imoted <tad.imokawa@gmail.com>
2023-10-04 17:12:20 +0900

    fix the test launch and script

--- ros_awsiot_agent/test/talker.py
+++ ros_awsiot_agent/test/talker.py
@@ -7,16 +7,18 @@ from std_msgs.msg import Header
 def talker():
     pub = rospy.Publisher("chatter", Header, queue_size=10)
     rospy.init_node("talker", anonymous=True)
-    rate = rospy.Rate(0.01)  # once in 10sec
+    rate = rospy.Rate(0.5)  # once in 2sec
     msg = Header()
+    seq_cnt = 0
     while not rospy.is_shutdown():
         hello_str = "Hello AWS IoT, greetings from ROS!"
         msg.frame_id = hello_str
         msg.stamp = rospy.get_rostime()
-        msg.seq = 10
+        msg.seq = seq_cnt
         rospy.loginfo(hello_str)
         pub.publish(msg)
         rate.sleep()
+        seq_cnt += 1
 
 
 if __name__ == "__main__":
